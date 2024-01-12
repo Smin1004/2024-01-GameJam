58574e6a0312c8cd456016c4648325494e5a14d4
Smin <sminnims1004@naver.com>
2024-01-12 13:21:45 +0900

    Setting

---
+++ Assets/Script/MoveManager.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+public class MoveManager : MonoBehaviour
+{
+    private static MoveManager _instance = null;
+    public static MoveManager Instance => _instance;
+
+    [SerializeField] private CameraMove cam;
+    public GameObject clearWin;
+
+    private int[,] curGroundMap;
+    private int[,] curObjMap;
+    private int[,] curMoveMap;
+    private MoveGround_Base[,] curMap;
+    private Enemy_Base[,] curMob;
+    private Obj_Base[,] curObj;
+    private List<MoveEnemy_Base> moveEnemy;
+    private List<MoveObj_Base> moveObj;
+    private List<MoveGround_Base> moveGround; //이동식 땅 임시 리스트
+    private List<MoveGround_Base> onOffGround;
+
+    public void Init()
+    {
+        _instance = this;
+    }
+
+    public void NextTiming()
+    {
+        foreach (var obj in moveObj) obj.NextTiming();
+        foreach (var enemy in moveEnemy) enemy.NextTiming();
+    }
+
+    public void OnOff()
+    {
+        foreach (var onoff in onOffGround) onoff.Use();
+    }
+
+    /// <summary>
+    /// return 0 == move, 1 == can't move, 2 == attack
+    /// </summary>
+    /// <param name="curPos"></param>
+    /// <param name="plusPos"></param>
+    /// <returns></returns>
+    public int MoveCheck(Vector2Int curPos, Vector2Int plusPos, bool isPlayer = false, bool isNotMove = false)
+    {
+        Vector2Int movePos = curPos + plusPos;
+
+        try { if (curGroundMap[movePos.x, movePos.y] == 0) { } /*map out check*/}
+        catch { return 1; }
+
+        if (curGroundMap[movePos.x, movePos.y] != 1)
+        {
+            if (curGroundMap[movePos.x, movePos.y] == 0) return 1;
+            else if (isPlayer && curGroundMap[movePos.x, movePos.y] == 99) curMap[movePos.x, movePos.y].Use();
+        }
+
+        if (curMoveMap[movePos.x, movePos.y] != 0)
+        {
+            if (isPlayer)
+            {
+                if (curMob[movePos.x, movePos.y].TryGetComponent<MoveEnemy_Base>(out var e))
+                {
+                    if (e.dirMove)
+                        if (e.cantMove) e.attackPos = curPos;
+                        else e.attackPos = movePos;
+                }
+                curMob[movePos.x, movePos.y].BackStep(plusPos);
+                return 2;
+            }
+            else
+            {
+                if (curMoveMap[movePos.x, movePos.y] == 1) return 2;
+                else return 1;
+            }
+        }
+        if (curObjMap[movePos.x, movePos.y] != 0)
+        {
+            if (isPlayer)
+                curObj[movePos.x, movePos.y].UseObj();
+
+            if (!curObj[movePos.x, movePos.y].isCanMove) return 1;
+        }
+
+        if (!isNotMove)
+        {
+            curMob[movePos.x, movePos.y] = curMob[curPos.x, curPos.y];
+            curMob[curPos.x, curPos.y] = null;
+
+            curMoveMap[movePos.x, movePos.y] = curMoveMap[curPos.x, curPos.y];
+            curMoveMap[curPos.x, curPos.y] = 0;
+        }
+
+        return 0;
+    }
+
+    public void InOutIndex(Vector2Int curPos, Define.MapType map, int index = 0)
+    {
+        switch (map)
+        {
+            case Define.MapType.Ground:
+                if (curGroundMap[curPos.x, curPos.y] == 0) curGroundMap[curPos.x, curPos.y] = index;
+                else curGroundMap[curPos.x, curPos.y] = 0;
+                break;
+
+            case Define.MapType.Obj:
+                if (curObjMap[curPos.x, curPos.y] == 0) curObjMap[curPos.x, curPos.y] = index;
+                else curObjMap[curPos.x, curPos.y] = 0;
+                break;
+        }
+    }
+
+    public void DestroyEnemy(Vector2Int curPos, MoveEnemy_Base enemy = null)
+    {
+        curMoveMap[curPos.x, curPos.y] = 0;
+        if (enemy != null) moveEnemy.Remove(enemy);
+    }
+
+    public void MapInit(MoveGround_Base[,] _curMap, int[,] _curGroundMap, List<MoveGround_Base> _moveGround, List<MoveGround_Base> _onoff)
+    {
+        curMap = _curMap;
+        curGroundMap = _curGroundMap;
+        moveGround = _moveGround;
+        onOffGround = _onoff;
+
+        curMoveMap = new int[_curGroundMap.GetLength(0), _curGroundMap.GetLength(1)];
+    }
+
+    public void MobInit(Enemy_Base[,] _map, Obj_Base[,] _obj, List<MoveObj_Base> _moveObj, List<MoveEnemy_Base> _moveEnemy, int[,] _curObjMap)
+    {
+        curObjMap = _curObjMap;
+        curMob = _map;
+        curObj = _obj;
+
+        moveObj = _moveObj.ToList();
+        moveEnemy = _moveEnemy.ToList();
+        cam.mapSize = new int[_curObjMap.GetLength(0), _curObjMap.GetLength(1)];
+
+        for (int i = 0; i < _map.GetLength(0); i++)
+            for (int j = 0; j < _map.GetLength(1); j++)
+            {
+                if (_map[i, j] != null) curMoveMap[i, j] = 2;
+            }
+
+        for (int i = 0; i < _curObjMap.GetLength(0); i++) for (int j = 0; j < _curObjMap.GetLength(1); j++)
+                if (_curObjMap[i, j] == 1)
+                {
+                    curMoveMap[i, j] = 1;
+                    Player.Instance.transform.position = new Vector3(i, 0, j);
+                    Player.Instance.curPos = new Vector2Int(i, j);
+                }
+
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class Player : Mob_Base
+{
+    private static Player _instance = null;
+    public static Player Instance => _instance;
+
+    [SerializeField] private AudioClip moveSound;
+    [SerializeField] private AudioClip attackSound;
+    [SerializeField] private AudioClip dieSound;
+    [SerializeField] private TextMeshProUGUI hpbar;
+
+    [SerializeField] private int HP;
+    [SerializeField] private bool isEasy;
+
+    public int isKey;
+    public bool isLobby;
+    public bool isDead;
+
+    private Vector2Int plusPos;
+    public bool cantMove;
+
+    public void Init()
+    {
+        _instance = this;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        isDead = false;
+        cantMove = false;
+    }
+
+    public void Setting(int hp)
+    {
+        isEasy = RoundData.Instance.isEasy;
+
+        if (!isEasy)
+        {
+            HP = hp;
+            hpbar.text = HP.ToString();
+        }
+        else hpbar.text = new("∞");
+    }
+
+    public void Damage(bool isAttack)
+    {
+        if (!isEasy)
+        {
+            HP -= 1;
+            hpbar.text = HP.ToString();
+        }
+        else return;
+
+        if (HP <= 0)
+        {
+            HP = 0;
+            DieDestroy();
+        }
+    }
+
+    public void Move(string type)
+    {
+        if (isDead || cantMove) return;
+
+        else switch (type)
+            {
+                case "Up": CheckMove(Vector3.forward); break;
+                case "Down": CheckMove(Vector3.back); break;
+                case "Right": CheckMove(Vector3.right); break;
+                case "Left": CheckMove(Vector3.left); break;
+            }
+    }
+
+    private void CheckMove(Vector3 movePos)
+    {
+        Vector2Int _plusPos = Vector2Int.zero;
+
+        if (movePos == Vector3.forward) _plusPos = Vector2Int.up;
+        if (movePos == Vector3.back) _plusPos = Vector2Int.down;
+        if (movePos == Vector3.right) _plusPos = Vector2Int.right;
+        if (movePos == Vector3.left) _plusPos = Vector2Int.left;
+
+        plusPos = _plusPos;
+
+        RookPlayer(movePos);
+        Managers.Sound.Play(moveSound);
+
+        int action = MoveManager.Instance.MoveCheck(curPos, plusPos, true);
+
+        switch (action)
+        {
+            case 0: curPos = curPos + plusPos; StartCoroutine(MovePlayer(movePos, 0.2f)); break;
+            case 1: break;
+            case 2: Attack(); break;
+        }
+    }
+
+    public void ForcedMovement()
+    {
+        Vector3 movePos = new();
+
+        if (plusPos == Vector2Int.up) movePos = Vector3.forward;
+        if (plusPos == Vector2Int.down) movePos = Vector3.back;
+        if (plusPos == Vector2Int.right) movePos = Vector3.right;
+        if (plusPos == Vector2Int.left) movePos = Vector3.left;
+
+        int action = MoveManager.Instance.MoveCheck(curPos, plusPos, true);
+
+        switch (action)
+        {
+            case 0:
+                Managers.Sound.Play(moveSound);
+                curPos = curPos + plusPos;
+                StartCoroutine(MovePlayer(movePos, 0.2f)); break;
+            case 1: cantMove = false; break;
+            case 2: cantMove = false; break;
+        }
+    }
+
+    void RookPlayer(Vector3 pos)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(pos), 1f);
+    }
+
+    void Attack()
+    {
+        Managers.Sound.Play(attackSound);
+        anim.SetTrigger("doAttack");
+    }
+
+    protected override void DieDestroy()
+    {
+        Managers.Sound.Play(dieSound);
+        anim.SetTrigger("doDeath");
+        isDead = true;
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction, float sec)
+    {
+        float elapsedTime = 0;
+        Vector3 origPos = transform.position;
+        Vector3 targetPos = origPos + direction;
+        anim.SetTrigger("doDash");
+
+        while (elapsedTime < sec)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, elapsedTime / sec);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+    }
+}

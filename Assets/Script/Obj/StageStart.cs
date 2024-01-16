25fd6e815fb90d60ef7e88d67ce4f3a6bfefe1ac
adyu1121 <122338805+adyu1121@users.noreply.github.com>
2024-01-16 23:48:47 +0900

    적 처치 이펙트

--- Assets/Script/Obj/StageStart.cs
+++ Assets/Script/Obj/StageStart.cs
@@ -1,10 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StageStart : Obj_Base
 {
     public int stage;
+    [SerializeField] Image _image;
+
     public override void UseObj()
     {
+        int clearStage = PlayerPrefs.GetInt("clearStage", 0);
+        if((clearStage + 1) > stage)
+        {
+
+        }
+        else
+        {
+
+        }
         MoveManager.Instance.curPlayer.allStop = true;
         FadeManager fade = FindAnyObjectByType<FadeManager>();
         fade?.Skip();

24c459ecee1eaec9b7d7d52961edff028d3bb190
A-Dyu <adyu1121@gmail.com>
2024-01-16 12:21:49 +0900

    character

--- Assets/Script/_01_ChapterIndex/_01_ChapterIndex.cs
+++ Assets/Script/_01_ChapterIndex/_01_ChapterIndex.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class _01_ChapterIndex : MonoBehaviour
 {
     [SerializeField] FadeManager _fade;
+    [SerializeField] Setting _setting;
     private void Start()
     {
         _fade.FadeIn(0.5f);
-        Invoke("SceneStart", 1);
+        //Invoke("SceneStart", 1);
     }
     void SceneStart()
     {
         SceneManager.LoadScene("TestScene");
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _fade.Skip();
+            _setting.OnOffSetting();
+        }
     }
 }
